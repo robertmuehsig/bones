a77bcec2ecbf3effa2c5ef0f1681a3e2cb4eb80f
Terence Eden <terence.eden@gmail.com>
2014-12-25 11:14:24 +0000

    Better gravatar support for mobile

--- functions.php
+++ functions.php
@@ -252,7 +252,7 @@ function bones_comments( $comment, $args, $depth ) {
           data-gravatar="<?php echo $protocol ?>www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=64" 
           class="load-gravatar avatar photo" 
           height="64" width="64" 
-          src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" 
+          src="<?php echo $protocol ?>www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=64" 
           alt="A picture of <?php echo get_comment_author(); ?>"
         />
         <?php // end custom gravatar call ?>
